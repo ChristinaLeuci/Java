8d1fcd07caba7fef61a818f5b4d24b72001f9ff0
christinaleuci <christinaleuci@gmail.com>
2014-09-28 02:53:15 -0400

    Victory practice program

---
+++ Victory.java
@@ -0,0 +1,16 @@
+public class Victory{
+  public static void main(String[] args){
+    drawForward();
+    words();
+    drawBackward();
+  }
+  public static void drawForward(){
+    System.out.println("///////////////////");
+  }
+  public static void drawBackward(){
+    System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+  }
+  public static void words(){
+    System.out.println("||Victory is mine||");
+  }
+}
