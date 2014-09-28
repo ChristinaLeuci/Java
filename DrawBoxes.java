875faab6d96497712acb4e7630051030593114af
christinaleuci <christinaleuci@gmail.com>
2014-09-28 01:40:26 -0400

    Difference between Static Methods and Main Methods

---
+++ DrawBoxes.java
@@ -0,0 +1,11 @@
+public class DrawBoxes{
+  public static void main(String[] args) {
+    drawBox();
+  }
+  public static void drawBox(){
+    System.out.println("+------+");
+    System.out.println("|      |");
+    System.out.println("|      |");
+    System.out.println("+------+");
+  }
+}
