b6c8f25e13be511e495391e6b1175eb22f7aafa3
christinaleuci <christinaleuci@gmail.com>
2014-09-28 02:52:48 -0400

    Slash practivce program

---
+++ Slash.java
@@ -0,0 +1,28 @@
+public class Slash{
+  public static void main(String[] args){
+    first();
+    second();
+    third();
+    fourth();
+    fifth();
+    sixth();
+  }
+  public static void first(){
+    System.out.println("  \\/");
+  }
+  public static void second(){
+    System.out.println(" \\\\//");
+  }
+  public static void third(){
+    System.out.println("\\\\\\///");
+  }
+  public static void fourth(){
+    System.out.println("///\\\\\\");
+  }
+  public static void fifth(){
+    System.out.println(" //\\\\");
+  }
+  public static void sixth(){
+    System.out.println("  /\\");  
+  }
+}
