909cf545fafab2b73a675215a072bc4645394e96
christinaleuci <christinaleuci@gmail.com>
2014-09-29 17:44:17 -0400

    multiple parameters and strings

---
+++ Objects/BusSong.java
@@ -0,0 +1,15 @@
+public class BusSong{
+  public static void main(String[] args){
+    verse("wheels", "go", "round and round");
+    verse("wipers","go", "swish, swish, swish");
+    verse("horn", "goes", "beep, beep, beep");
+  }
+  public static void verse(String item, String verb, String sound){
+    System.out.println("The " + item + " on the bus go " + verb + " " + sound + ", ");
+    System.out.println(sound + ", ");
+    System.out.println(sound + ". ");
+    System.out.println("The " + item + " on the bus go " + verb + " " + sound + ", ");
+    System.out.println("All through the town.");
+    System.out.println();
+  }
+}
